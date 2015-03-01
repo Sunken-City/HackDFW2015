6f26069fcb21555e8dc9cc357ca8dd1fb80582d7
Pico Riley <acloudy@mail.smu.edu>
2015-02-28 23:29:14 -0500

    Added the target

---
+++ UltimateFunBall/Assets/destroyOnCollide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyOnCollide : MonoBehaviour {
+
+	public AudioClip breakSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (breakSound) {
+			AudioSource.PlayClipAtPoint (breakSound, Vector3.zero);
+		}
+		Debug.Log ("Destroyin'");	
+		Destroy (this.gameObject);
+	}
+}
